d1fbf32bcc2bfd4a1177597e4c8f076f706a76e3
Matthew Machivenyika <mm1079@c2i.ch>
2023-04-14 15:22:49 +0200

    Initial commit

---
+++ windowsca/provider.go
@@ -0,0 +1,47 @@
+package windowsca
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+)
+
+// Ensure the implementation satisfies the expected interfaces
+var (
+	_ provider.Provider = &windowscaProvider{}
+)
+
+// New is a helper function to simplify provider server and testing implementation.
+func New() provider.Provider {
+	return &windowscaProvider{}
+}
+
+// windowscaProvider is the provider implementation.
+type windowscaProvider struct{}
+
+// Metadata returns the provider type name.
+func (p *windowscaProvider) Metadata(_ context.Context, _ provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "windowsca"
+}
+
+// Schema defines the provider-level schema for configuration data.
+func (p *windowscaProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp *provider.SchemaResponse) {
+	resp.Schema = schema.Schema{}
+}
+
+// Configure prepares a Windows WinRM client for data sources and resources.
+func (p *windowscaProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+}
+
+// DataSources defines the data sources implemented in the provider.
+func (p *windowscaProvider) DataSources(_ context.Context) []func() datasource.DataSource {
+	return nil
+}
+
+// Resources defines the resources implemented in the provider.
+func (p *windowscaProvider) Resources(_ context.Context) []func() resource.Resource {
+	return nil
+}
